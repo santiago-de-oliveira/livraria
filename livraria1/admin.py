b0b480dfbd409e4e3d8f1da2065d5f71130c782e
Santiago de Oliveira <santiagodeoliveira2004@gmail.com>
2024-03-19 14:37:52 -0300

    API REST DRF

--- livraria1/admin.py
+++ livraria1/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Categoria, Editora
+from .models import Categoria, Editora, Autor, Livro
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
--- livraria1/views.py
+++ livraria1/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.viewsets import ModelViewSet
+from livraria1.models import Categoria
+from livraria1.serializers import CategoriaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
